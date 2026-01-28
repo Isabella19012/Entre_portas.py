57bad6a2384732e1710e2bb30cce6b0c657bb6d7
Isabella <isabella.o@escolar.ifrn.edu.br>
2026-01-27 21:22:15 -0300

    implementação da fase 1(quase concluida); fundo da fase 2 ja lá.

--- jogo.py
+++ jogo.py
@@ -13,7 +13,6 @@ fonte_txt=pygame.font.Font(None, 40)
 subtitulo_fonte = pygame.font.Font(None, 23)
 
 subtitulo = subtitulo_fonte.render(f'As escolham importam', True, 'White')
-
 surface_texto = fonte_bemvindo.render(f"Entre Portas", True, 'White')
 
 voltar = fonte_txt.render(f'<-', True, (192, 79, 21))
@@ -37,13 +36,18 @@ perdeu = pygame.image.load('png/voceperdeu.png')
 perdeu = pygame.transform.scale(perdeu, (x, y)) 
 #fundo instrução
 fundo_instrucao = pygame.image.load("png/fundo instrução2.png")
-fundo_instrucao = pygame.transform.scale(fundo_instrucao, (1000, 500))
+fundo_instrucao = pygame.transform.scale(fundo_instrucao, (x, y))
 fundo_instrucao = fundo_instrucao.convert_alpha()
-#porta errada
-porta_errada = pygame.image.load('jpg/porta_da_primeira_fase.jpg')
-porta_errada = pygame.transform.scale(porta_errada, (60, 100))
-porta_errada_rect = porta_errada.get_rect()
-porta_errada_rect.topleft = (400, 250)
+#porta errada e certa
+porta_certa1 = pygame.image.load('png/porta_primeira fase_estrela.png')
+porta_certa1 = pygame.transform.scale(porta_certa1, (75, 130))
+porta_certa1_rect = porta_certa1.get_rect()
+porta_certa1_rect.topleft = (10, 270)
+
+porta_errada1 = pygame.image.load('jpg/porta_da_primeira_fase.jpg')
+porta_errada1 = pygame.transform.scale(porta_errada1, (75, 130))
+porta_errada_rect = porta_errada1.get_rect()
+porta_errada_rect.topleft = (905, 270)
 #perdeu, fase 1 e novamente.
 botao_fase1 = pygame.image.load('png/voltar_fase1.png')
 botao_fase1_rect = botao_fase1.get_rect()
@@ -53,10 +57,20 @@ botao_voltar = pygame.image.load('png/voltar_menu.png')
 botao_voltar_rect = botao_voltar.get_rect()
 botao_voltar_rect.topleft = (550, 300)
 
+# fundo das fases
+fundo_fase1 = pygame.image.load('png/fundo_fase1.png')
+fundo_fase1 = pygame.transform.scale(fundo_fase1, (x, y))
+fundo_fase1 = fundo_fase1.convert_alpha()
+
+fundo_fase2 = pygame.image.load('png/cenario_fase2.png')
+fundo_fase2 = pygame.transform.scale(fundo_fase2, (x, y))
+fundo_fase2 = fundo_fase2.convert_alpha()
 #desistir
 desistir = fonte_txt.render(f'Menu <-', True, 'White')
 desistir_rect = desistir.get_rect()
-desistir_rect.topleft = (800,30)
+desistir_rect.topleft = (850,30)
+
+
 while running:
   
   for event in pygame.event.get():
@@ -80,20 +94,9 @@ while running:
            tela = 'tela_menu'
         elif desistir_rect.collidepoint(event.pos):
           tela = "tela_menu"
+        elif porta_certa1_rect.collidepoint(event.pos):
+          tela = 'fase2'
 
-
-        #elif fase1_rect.collidepoint(event.pos):
-         #  tela ='fase1'
-        #elif fase2_rect.collidepoint(event.pos):
-          # tela ='fase1'
-        #elif fase3_rect.collidepoint(event.pos):
-          # tela ='fase5'
-        #elif fase4_rect.collidepoint(event.pos):
-          # tela = 'fase4'
-        #elif fase5_rect.collidepoint(event.pos):
-          # tela = 'fase5'
-        #elif voceperdeu_rect.collidepoint(event.pos):
-          # tela = 'voceperdeu'
            
     if tela == 'tela_menu':
 
@@ -112,19 +115,22 @@ while running:
 
     if tela == "click_jogar":
         tela = 'fase1'
+    elif tela == "click_instrucoes":
+        
+      screen.blit(fundo_instrucao, (0,0))
+      screen.blit(voltar, voltar_rect.topleft)
 
     if tela == 'fase1':
-      screen.fill((0, 0, 0))
-      screen.blit(porta_errada, porta_errada_rect.topleft)
+      screen.blit(fundo_fase1,  (0,0))
+      screen.blit(porta_errada1, porta_errada_rect.topleft)
+      screen.blit(porta_certa1, porta_certa1_rect)
       screen.blit(desistir, desistir_rect.topleft)
     if tela == 'perdeu':
       screen.blit(perdeu, (0, 0))
       screen.blit(botao_voltar, botao_voltar_rect.topleft)
       screen.blit(botao_fase1, botao_fase1_rect.topleft)
-
-    elif tela == "click_instrucoes":
-        
-      screen.blit(fundo_instrucao, (0,0))
+    if tela == 'fase2':
+      screen.blit(fundo_fase2, (0,0))
       screen.blit(voltar, voltar_rect.topleft)
         
     pygame.display.flip() 
