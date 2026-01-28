c546cb357cb93fb6c08bc338aa0e76e882e99348
Isabella <isabella.o@escolar.ifrn.edu.br>
2026-01-28 17:57:37 -0300

    mudar o curso em um quando colidir e botão fantasma resolvido

--- jogo.py
+++ jogo.py
@@ -69,71 +69,108 @@ fundo_fase2 = fundo_fase2.convert_alpha()
 desistir = fonte_txt.render(f'Menu <-', True, 'White')
 desistir_rect = desistir.get_rect()
 desistir_rect.topleft = (850,30)
-
+#dicas:
+dica_gato = pygame.image.load('png/gatinha.png')
+dicag_rect = dica_gato.get_rect
 
 while running:
-  
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        running = False
-    elif event.type == pygame.MOUSEBUTTONDOWN:
-    # Check if the left button was clicked (button 1)
-       if event.button == 1:
-       # Check for collision between mouse position and the object's Rect
-        if texto2_rect.collidepoint(event.pos):
-          tela = "click_jogar"
-        elif texto3_rect.collidepoint(event.pos):
-          tela =  'click_instrucoes'
-        elif voltar_rect.collidepoint(event.pos):
-          tela = "tela_menu"
-        elif porta_errada_rect.collidepoint(event.pos):
-          tela = 'perdeu'
-        elif botao_fase1_rect.collidepoint(event.pos):
-           tela = 'fase1'
-        elif botao_voltar_rect.collidepoint(event.pos):
-           tela = 'tela_menu'
-        elif desistir_rect.collidepoint(event.pos):
-          tela = "tela_menu"
-        elif porta_certa1_rect.collidepoint(event.pos):
-          tela = 'fase2'
 
-           
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+
+            if tela == 'tela_menu':
+                if texto2_rect.collidepoint(event.pos):
+                    tela = 'fase1'
+                elif texto3_rect.collidepoint(event.pos):
+                    tela = 'click_instrucoes'
+
+            elif tela == 'click_instrucoes':
+                if voltar_rect.collidepoint(event.pos):
+                    tela = 'tela_menu'
+
+            elif tela == 'fase1':
+                if porta_errada_rect.collidepoint(event.pos):
+                    tela = 'perdeu'
+                elif porta_certa1_rect.collidepoint(event.pos):
+                    tela = 'fase2'
+                elif desistir_rect.collidepoint(event.pos):
+                    tela = 'tela_menu'
+
+            elif tela == 'perdeu':
+                if botao_voltar_rect.collidepoint(event.pos):
+                    tela = 'tela_menu'
+                elif botao_fase1_rect.collidepoint(event.pos):
+                    tela = 'fase1'
+
+            elif tela == 'fase2':
+                if voltar_rect.collidepoint(event.pos):
+                    tela = 'tela_menu'
+
+    mouse_pos = pygame.mouse.get_pos()
+
+#TELAS
     if tela == 'tela_menu':
+        screen.blit(fundo, (0, 0))
+        pygame.draw.rect(screen, (226, 81, 0), (0, 0, x, 100))
+        pygame.draw.rect(screen, (226, 81, 0), (0, 400, x, 120))
+        screen.blit(subtitulo, (405, 70))
+        screen.blit(surface_texto, (380, 30))
+        pygame.draw.rect(screen, (255, 255, 255), (230, 410, 230, 70), 2, 5)
+        pygame.draw.rect(screen, (255, 255, 255), (510, 410, 230, 70), 2, 5)
+        screen.blit(texto2, texto2_rect)
+        screen.blit(texto3, texto3_rect)
 
-      screen.blit(fundo, (0, 0)) #0,0, 153 - tela fase 1
-      pygame.draw.rect(screen, (226, 81, 0), (0, 0, x, 100))
-      pygame.draw.rect(screen, (226, 81, 0), (0, 400, x, 120))
-      #subtitulo
-      screen.blit(subtitulo, (405, 70))
-      screen.blit(surface_texto, (380, 30))  
-      #bordas do >botao<
-      pygame.draw.rect(screen, (255, 255, 255), (230, 410, 230, 70), (border_radius:=5))
-      pygame.draw.rect(screen, (255, 255, 255), (510, 410, 230, 70), (border_radius:=5))
+        if texto2_rect.collidepoint(mouse_pos) or texto3_rect.collidepoint(mouse_pos):
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+        else:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
-      screen.blit(texto2, texto2_rect.topleft)  
-      screen.blit(texto3, texto3_rect.topleft) 
 
-    if tela == "click_jogar":
-        tela = 'fase1'
-    elif tela == "click_instrucoes":
+    elif tela == 'click_instrucoes':
+        screen.blit(fundo_instrucao, (0, 0))
+        screen.blit(voltar, voltar_rect)
         
-      screen.blit(fundo_instrucao, (0,0))
-      screen.blit(voltar, voltar_rect.topleft)
+        if voltar_rect.collidepoint(mouse_pos):
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+        else:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
-    if tela == 'fase1':
-      screen.blit(fundo_fase1,  (0,0))
-      screen.blit(porta_errada1, porta_errada_rect.topleft)
-      screen.blit(porta_certa1, porta_certa1_rect)
-      screen.blit(desistir, desistir_rect.topleft)
-    if tela == 'perdeu':
-      screen.blit(perdeu, (0, 0))
-      screen.blit(botao_voltar, botao_voltar_rect.topleft)
-      screen.blit(botao_fase1, botao_fase1_rect.topleft)
-    if tela == 'fase2':
-      screen.blit(fundo_fase2, (0,0))
-      screen.blit(voltar, voltar_rect.topleft)
-        
-    pygame.display.flip() 
-    clock.tick(60)  
+    
+
+    elif tela == 'fase1':
+        screen.blit(fundo_fase1, (0, 0))
+        screen.blit(porta_errada1, porta_errada_rect)
+        screen.blit(porta_certa1, porta_certa1_rect)
+        screen.blit(desistir, desistir_rect)
+
+        if porta_certa1_rect.collidepoint(mouse_pos) or porta_errada_rect.collidepoint(mouse_pos) or desistir_rect.collidepoint(mouse_pos):
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+        else:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+
+
+    elif tela == 'perdeu':
+        screen.blit(perdeu, (0, 0))
+        screen.blit(botao_voltar, botao_voltar_rect)
+        screen.blit(botao_fase1, botao_fase1_rect)
+        if botao_voltar_rect.collidepoint(mouse_pos) or botao_fase1_rect.collidepoint(mouse_pos):
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+        else:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+
+    elif tela == 'fase2':
+        screen.blit(fundo_fase2, (0, 0))
+        screen.blit(voltar, voltar_rect)
+
+        if voltar_rect.collidepoint(mouse_pos):
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+        else:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+
+    pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
